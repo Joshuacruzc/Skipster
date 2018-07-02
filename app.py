64b65c46eb989cee4ac57a690c2e1872563961ea
javierbustillo <javier.bustillo@upr.edu>
2018-07-02 19:11:57 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from flask import Flask
+from flask import Flask, request, redirect
 import spotipy
+from spotipy import util
+import urllib
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def initial():
-    if request.method == 'GET:'
-        return 'Skipster \nGive us access to your account :)'
+    pass
+
+@app.route('/register', methods = ['GET'])
+def register():
+    return redirect('https://accounts.spotify.com/authorize/?client_'
+                 'id=e0147040c0e24397a075519bb718324d&response_type='
+                 'code&redirect_uri=http://localhost:5000/index&'
+                 'scope=user-read-private%20user-read-email&state=34fFs29kd09')
+
+@app.route('/index')
+def callback():
+    params = urllib.urlencode({'grant_type':"authorization_code", 'code': request.args.get('code'), 'redirect_uri':
+        'http:localhost:5000/index'})
+    token = urllib.urlopen("https://accounts.spotify.com/api/token", params)
+    print(token)
+
+    return str(token) + 'lol'
+
 
 
 if __name__ == '__main__':
