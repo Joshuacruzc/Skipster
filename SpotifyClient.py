73e4bc103cfaefb96cda60064f41e1d15c5f2198
javierbustillo <javier.bustillo@upr.edu>
2018-07-09 19:23:26 -0400

    User playlist can now be created

--- SpotifyClient.py
+++ SpotifyClient.py
@@ -1,10 +1,13 @@
 import requests, base64
+from flask import jsonify
+import json
 
 
 def create_playlist(token, user_id, name):
     #Description: Made by skipster, link al website
 
-    url = 'https//api.spotify.com/v1/users/%s/playlists' % user_id
+    url = 'https://api.spotify.com/v1/users/%s/playlists' % user_id
     params = {'name': name, 'description': 'Link al website and promotional gimmicks'}
-    response = requests.post(url, headers={"Content-type": "application/json", "Authorization": token}, params=params)
+    response = requests.post(url, headers={"Authorization": 'Bearer ' + token, "Content_type": 'application/json'},
+                             data=json.dumps(params))
     return response
--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, redirect
 import requests, base64
 from SpotifyClient import *
+import json
+import ast
 
 app = Flask(__name__)
 
@@ -21,9 +23,10 @@ def callback():
     params = {'grant_type': "authorization_code", 'code': request.args.get('code'), 'redirect_uri':
         'http://localhost/index',"client_id": '2fb54226ea3f4a748b823a0ce06704c2', "client_secret":'5f57b22c53294d54befc2dd5667c1268' }
     b64Val = base64.urlsafe_b64encode(b"2fb54226ea3f4a748b823a0ce06704c2:5f57b22c53294d54befc2dd5667c1268")
-    token = requests.post('https://accounts.spotify.com/api/token', headers={"Content-type": "application/x-www-form-urlencoded"},params=params)
+    response = requests.post('https://accounts.spotify.com/api/token', headers={"Content-type": "application/x-www-form-urlencoded"},params=params)
 
-    return create_playlist(token, '1215638892', 'Skipster Prototype')
+    dict = ast.literal_eval(response.text)
+    return create_playlist(dict['access_token'], '1215638892', 'Skipster Prototype').content
 
 
 
