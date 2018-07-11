c2581d690b3bcb2b44d13f837cc9671b4e9e55a0
Joshua <joshua.cruz15@upr.edu>
2018-07-11 00:01:15 -0400

    Begun writing backend for registration and login, simple templates added

--- SpotifyClient.py
+++ SpotifyClient.py
@@ -1,7 +1,14 @@
 import requests, base64
-from flask import jsonify
+from flask import redirect
 import json
 
+
+
+def get_token():
+    return redirect('https://accounts.spotify.com/authorize/?client_'
+             'id=2fb54226ea3f4a748b823a0ce06704c2&response_type='
+             'code&redirect_uri=http://localhost/index&'
+             'scope=playlist-modify-public%20user-read-email&state=34fFs29kd09')
 
 def create_playlist(token, user_id, name):
     #Description: Made by skipster, link al website
